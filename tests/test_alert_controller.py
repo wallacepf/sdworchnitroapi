b5b7a7556098ff3a1d2f41abd6d1ded179d43119
wallacepf <wallacepf@gmail.com>
2020-06-25 22:18:41 -0300

    added DelAlert scope

--- tests/test_alert_controller.py
+++ tests/test_alert_controller.py
@@ -3,6 +3,7 @@ from sdworchnitroapi import auth_controller
 
 import os
 import json
+import pytest
 
 cc_id = os.environ['CCID']
 client_id = os.environ['CLIENT_ID']
@@ -29,19 +30,34 @@ class GetAlerts:
             alerts.get_alerts(context='customer').keys())
 
     def test_get_alerts_site(self):
-        pass
+        json_return = json.dumps(alerts.get_alerts(context='site'))
+        for each in key_list:
+            assert each in json_return['alerts'][0]
+        assert 'totalElements' in json_return
+        assert len(key_list) == len(
+            alerts.get_alerts(context='customer').keys())
 
     def test_get_alerts_msp(self):
-        pass
-
+        json_return = json.dumps(alerts.get_alerts(context='msp'))
+        for each in key_list:
+            assert each in json_return['alerts'][0]
+        assert 'totalElements' in json_return
+        assert len(key_list) == len(
+            alerts.get_alerts(context='customer').keys())
 
 class DelAlert:
+    @pytest.fixture
+    def get_alert_data_customer(self, context):
+        id_return = alerts.get_alerts(context=context)['alerts'][0]['id']
+        return id_return
+
     def test_del_alert_customer(self):
-        pass
+        assert alerts.del_alert(
+            context='customer', alert_id=self.get_alert_data_customer('customer')) == 'Done'
 
     def test_del_alert_msp(self):
-        pass
-
+        assert alerts.del_alert(
+            context='msp', alert_id=self.get_alert_data_customer('msp')) == 'Done'
 
 class DelAllAlerts:
     def test_del_all_alerts_customer(self):
